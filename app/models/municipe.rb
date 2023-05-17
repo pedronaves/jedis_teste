4bbb3697daaf4776f206f23df54bd364111c89ce
pedro naves <oi@pedronaves.com>
2023-05-17 18:09:43 -0300

    add validate to cns, cpf and email

--- app/models/municipe.rb
+++ app/models/municipe.rb
@@ -1,11 +1,24 @@
 # frozen_string_literal: true
 
 class Municipe < ApplicationRecord
+
+  include Cns
+
+  has_many :address, dependent: :destroy
+
   include ImageUploader::Attachment(:image)
   validates :name, :cpf, :cns, :email, :birthday, :phone_country, :phone_area, :phone_number, :image, presence: true
 
   validates_email_format_of :email, disposable: true
   validates_cpf_format_of :cpf
 
-  has_many :address, dependent: :destroy
+  validate :email_valid?, :cns_valid?
+
+  def email_valid?
+    return errors.add(:email, 'inválido') unless email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  end
+
+  def cns_valid?
+    return errors.add(:cns, 'inválido') unless Cns.validate(cns)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Jedisapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.autoload_paths += %W[#{config.root}/lib]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
