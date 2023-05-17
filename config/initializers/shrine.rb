409ea6b89a2f754fba9720a36a20d4c4bcbfd39e
pedro naves <oi@pedronaves.com>
2023-05-17 17:20:39 -0300

    fix image preview

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -4,17 +4,10 @@ require 'shrine'
 require 'shrine/storage/file_system'
 require 'shrine/storage/memory'
 
-Shrine.storages = if Rails.env.test?
-                    {
-                      cache: Shrine::Storage::Memory.new,
-                      store: Shrine::Storage::Memory.new
-                    }
-                  else
-                    {
-                      cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
-                      store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads') # permanent
-                    }
-                  end
+Shrine.storages = {
+  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
+  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent
+}
 
 Shrine.plugin :activerecord           # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
