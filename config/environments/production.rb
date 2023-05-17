8d8c373ee16a4c29a41e8fa884ba8f667c609ef4
pedro naves <oi@pedronaves.com>
2023-05-17 19:29:29 -0300

    fix mailer to development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,10 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "jedisapp_production"
 
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
+  config.active_job.queue_adapter = :inline
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
