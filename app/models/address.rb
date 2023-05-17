4f902ca805dabd3894f7c932bbb6ff8e7d11fd3f
pedro naves <oi@pedronaves.com>
2023-05-17 16:29:40 -0300

    removing wrong validates

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,5 +3,5 @@
 class Address < ApplicationRecord
   belongs_to :municipe
 
-  validates :postal_code, :address, :address_complement, :neighborhood, :city, :state, presence: true
+  validates :postal_code, :address, :neighborhood, :city, :state, presence: true
 end
